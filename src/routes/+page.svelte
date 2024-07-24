da06697c601eb655f4b3cf947300f7cd8fe5b763
Elliot Alderson <akshithkatkuri14@gmail.com>
2024-07-24 12:46:11 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  let name = "";
+  let greetMsg = "";
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<div class="container">
+  <div>
+    <button on:click|preventDefault={greet}>Greet</button>
+    <div>{greetMsg}</div>
+  </div>
+</div>

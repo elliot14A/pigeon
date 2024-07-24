778e1ac06a4807a3312ed26556aa36a05b72764b
Elliot Alderson <akshithkatkuri14@gmail.com>
2024-07-24 13:30:38 +0530

    add tailwindcss config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,17 @@
-<script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri";
-
-  let name = "";
-  let greetMsg = "";
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name });
-  }
-</script>
+<!-- <script lang="ts"> -->
+<!--   import { invoke } from "@tauri-apps/api/tauri"; -->
+<!---->
+<!--   let name = ""; -->
+<!--   let greetMsg = ""; -->
+<!---->
+<!--   async function greet() { -->
+<!--     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command -->
+<!--     greetMsg = await invoke("greet", { name }); -->
+<!--   } -->
+<!-- </script> -->
 
 <div class="container">
   <div>
-    <button on:click|preventDefault={greet}>Greet</button>
-    <div>{greetMsg}</div>
+    <p>Random text here</p>
   </div>
 </div>
